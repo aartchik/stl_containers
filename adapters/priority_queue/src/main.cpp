f4cb38ad5fa7fe8cd66ebc3da7ac87bd582aec9a
artchik_lin <artyr-nasredin@yandex.ru>
2025-09-12 00:39:13 +0300

    first init

---
+++ adapters/priority_queue/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "priority_queue.hpp"
+#
+
+
+int main()
+{
+    priority_queue<int> arr;
+    arr.push(5);
+    arr.push(10);
+    arr.push(101);
+    arr.push(2);
+    arr.push(18);
+    arr.push(-25);
+    
+
+    while (!arr.empty())
+    {
+        std::cout << arr.top() << std::endl;
+        arr.pop();
+
+    }
+
+
+}
